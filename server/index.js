82fae63d81be2e69e7c7cf9a1d77bbbdc11d8b95
faizan-purelogics <122025996+faizan-purelogics@users.noreply.github.com>
2023-09-04 19:36:29 +0500

    Added vercel.json for deployement

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const config = require('./config/config');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const config = require("./config/config");
 const PORT = config.PORT;
-require('./config/db');
-require('dotenv').config();
+require("./config/db");
+require("dotenv").config();
 
 app.use(cookieParser());
 app.use(cors());
@@ -15,18 +15,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Import routers
-const userRouter = require('./routers/userRoute');
-const tweetRouter = require('./routers/tweetRoute');
+const userRouter = require("./routers/userRoute");
+const tweetRouter = require("./routers/tweetRoute");
 
-app.use('/api/users', userRouter);
-app.use('/api/tweets', tweetRouter);
+app.use("/api/users", userRouter);
+app.use("/api/tweets", tweetRouter);
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Twitter clone backend!');
+app.get("/", (req, res) => {
+  res.send("Welcome to the Twitter clone backend!");
 });
 
 /** catch 404 and forward to error handler */
-app.use('*', (req, res) => {
+app.use("*", (req, res) => {
   return res.status(404).json({
     statusCode: 0,
     message: `API endpoint doesn't exist`,
