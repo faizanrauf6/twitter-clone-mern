228335b5d7a4f2f702796af5e2147f7c60eeec95
faizan-purelogics <122025996+faizan-purelogics@users.noreply.github.com>
2023-09-04 19:45:33 +0500

    Updated backend APIs base url

--- client/src/constants/config.js
+++ client/src/constants/config.js
@@ -1,6 +1,6 @@
 const config = {
-  base_url: 'http://localhost:4949',
-  time_out: 20 * 1000, // 10 sec
+  base_url: "https://twitter-clone-backend-indol.vercel.app",
+  time_out: 10 * 1000, // 10 sec
 };
 
 export default config;
